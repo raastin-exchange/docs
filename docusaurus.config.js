8b89471d2bbc0373f7389f8e0d4b8e862ea93406
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-09-20 15:50:16 +0330

    base version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: 'https://raastin-exchange.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/api/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -37,12 +37,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
---
+++ src/components/Json.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Json({text}){
+
+    return (<div style={{direction:"ltr"}}><h2>{text}</h2></div>)
+}
